c9900a1016455c776cee6ec543c5a1f3b16b3785
Akmuhammet Jorayev <jorayew.pro@gmail.com>
2026-01-07 01:34:41 +0200

    Fix CI runtime checks and toolchain setup

--- scripts/run_valgrind.sh
+++ scripts/run_valgrind.sh
@@ -5,9 +5,18 @@ BUILD_DIR="$1"
 
 # Если CI не передал EXECUTABLE_NAME, используем Ashyk
 EXECUTABLE_NAME=${EXECUTABLE_NAME:-Ashyk}
+INPUT_FILE=${INPUT_FILENAME:-}
 
-valgrind \
-  --leak-check=full \
-  --show-leak-kinds=all \
-  --error-exitcode=1 \
-  "./${BUILD_DIR}/${EXECUTABLE_NAME}"
+if [[ -n "${INPUT_FILE}" && -f "${INPUT_FILE}" ]]; then
+  tr -d '\r' < "${INPUT_FILE}" | valgrind \
+    --leak-check=full \
+    --show-leak-kinds=all \
+    --error-exitcode=1 \
+    "./${BUILD_DIR}/${EXECUTABLE_NAME}"
+else
+  valgrind \
+    --leak-check=full \
+    --show-leak-kinds=all \
+    --error-exitcode=1 \
+    "./${BUILD_DIR}/${EXECUTABLE_NAME}"
+fi
